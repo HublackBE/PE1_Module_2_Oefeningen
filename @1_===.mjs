7a166dd03d24c2705f4f52f46c5837a0cbec70c0
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-10-02 10:35:51 +0200

    @1_=== done

---
+++ @1_===.mjs
@@ -0,0 +1,10 @@
+let var1 = 42;
+console.log(var1);
+let var2 = '42';
+console.log('\'' + var2 + '\'');
+
+console.log('var1 == var2 ?', var1 == var2);
+console.log('var1 === var2 ?', var1 === var2);
+
+
+process.exit();
