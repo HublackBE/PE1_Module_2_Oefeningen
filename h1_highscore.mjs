5536a33d0ddaf58ab130eecedca1b5c121391374
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-10-09 10:03:29 +0200

    Perfected h1_highscore Combined different for-loops in one Removed array as it is now unnecessary

--- h1_highscore.mjs
+++ h1_highscore.mjs
@@ -2,37 +2,25 @@ import * as readline from 'node:readline/promises';
 import {stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface(input, output);
 
-let numbers = [];
+
+let sum = 0;
+let highest;
+let lowest;
 
 for(let i = 0; i <= 4; i++) {
     let numInput = parseFloat(await userInput.question('Nummer: '));
-    numbers.push(numInput);
-}
-
-let highestNum = numbers[0];
-
-for(let i = 1; i <= 4; i++){
-    if(numbers[i] > highestNum){
-        highestNum = numbers[i];
+    sum += numInput;
+    if(numInput > highest || highest === undefined) {
+        highest = numInput;
+    }
+    if(numInput < lowest || lowest === undefined) {
+        lowest = numInput;
     }
 }
-console.log('Het hoogste getal is', highestNum);
 
-let lowestNum = numbers[0];
-
-for(let i = 1; i < 5; i++){
-    if(numbers[i] < lowestNum){
-        lowestNum = numbers[i];
-    }
-}
-console.log('Het laagste getal is', lowestNum);
-
-let sum = 0;
-
-for(let i = 0; i < numbers.length; i++){
-    sum += numbers[i];
-}
-console.log('Het gemiddelde is', sum / 5);
+console.log('Lowest number is', lowest);
+console.log('Highest number is', highest);
+console.log('Average is', sum / 5);
 
 
 process.exit();
